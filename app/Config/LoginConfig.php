dd131a3955218dd93fecac2eda3e63ba95b804a2
Sentrakom <danil@sentrakom-binatama.com>
2024-12-22 18:37:52 +0700

    update max atemps 3

--- app/Config/LoginConfig.php
+++ app/Config/LoginConfig.php
@@ -6,6 +6,6 @@ use CodeIgniter\Config\BaseConfig;
 
 class LoginConfig extends BaseConfig
 {
-  public $maxAttempts = 5;      // Maximum allowed attempts
+  public $maxAttempts = 3;      // Maximum allowed attempts
   public $lockoutTime = 300;    // Lockout time in seconds (5 minutes)
 }
